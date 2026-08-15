a5d5f3274d472c2e1bc76c865160fd9f4438d6d6
veenyi <60565189+veenyi@users.noreply.github.com>
2026-08-15 10:15:46 +0800

    DSH Desktop v0.3.0 source

---
+++ electron/shell-preload.js
@@ -0,0 +1,47 @@
+'use strict';
+// 主工作台窗口 preload：暴露壳层能力到 dsh web UI（页面来自 127.0.0.1，跨进程桥）
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('__dshShell', {
+  openSettingsFile: () => ipcRenderer.invoke('shell:openSettingsFile'),
+  openDataDir: () => ipcRenderer.invoke('shell:openDataDir'),
+  openLogsDir: () => ipcRenderer.invoke('shell:openLogsDir'),
+  openMarket: () => ipcRenderer.invoke('shell:openMarket'),
+  openWelcome: () => ipcRenderer.invoke('shell:openWelcome'),
+  openSettings: () => ipcRenderer.invoke('shell:openSettingsWindow'),
+  openWorkspace: () => ipcRenderer.invoke('shell:openWorkspace'),
+  openBrowser: () => ipcRenderer.invoke('shell:openBrowser'),
+  openExternal: (url) => ipcRenderer.invoke('shell:openExternal', url),
+  takeSnapshot: () => ipcRenderer.invoke('shell:takeSnapshot'),
+  getVersion: () => ipcRenderer.invoke('shell:getVersion'),
+  listSkills: () => ipcRenderer.invoke('shell:listSkills'),
+  listArtifacts: () => ipcRenderer.invoke('shell:listArtifacts'),
+  listWorkspaces: () => ipcRenderer.invoke('shell:listWorkspaces'),
+  serverStatus: () => ipcRenderer.invoke('shell:serverStatus'),
+  channelStatus: () => ipcRenderer.invoke('shell:channelStatus')
+});
+
+// 设置窗口 preload 桥
+contextBridge.exposeInMainWorld('settings', {
+  get: () => ipcRenderer.invoke('settings:get'),
+  setAutoLaunch: (v) => ipcRenderer.invoke('settings:setAutoLaunch', v),
+  setOpenWith: (v) => ipcRenderer.invoke('settings:setOpenWith', v),
+  setHotkey: (v) => ipcRenderer.invoke('settings:setHotkey', v),
+  setDingtalk: (k, s) => ipcRenderer.invoke('settings:setDingtalk', k, s),
+  // IM 渠道
+  getChannels: () => ipcRenderer.invoke('settings:getChannels'),
+  saveChannel: (id, values) => ipcRenderer.invoke('settings:saveChannel', id, values),
+  clearChannel: (id) => ipcRenderer.invoke('settings:clearChannel', id),
+  // 更新中心
+  checkUpdate: () => ipcRenderer.invoke('shell:checkUpdate'),
+  updateState: () => ipcRenderer.invoke('shell:updateState'),
+  updateDownload: () => ipcRenderer.invoke('shell:updateDownload'),
+  updateInstall: () => ipcRenderer.invoke('shell:updateInstall'),
+  updateSource: (patch) => ipcRenderer.invoke('shell:updateSource', patch),
+  updateOpenDir: () => ipcRenderer.invoke('shell:updateOpenDir'),
+  onUpdateState: (cb) => {
+    const listener = (_e, state) => cb(state);
+    ipcRenderer.on('update:state', listener);
+    return () => ipcRenderer.removeListener('update:state', listener);
+  }
+});
